4b8b9fe22ba59e8fe6c223049c052e9fc1601d70
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2026-03-10 11:44:19 +0545

    `refactor: remove chat component index`

--- components/chat/index.ts
+++
@@ -1,4 +0,0 @@
-export { ChatInput } from "./chat-input";
-export { MessageList } from "./message-list";
-export { ToolCallItem } from "./tool-call-item";
-export { ScrollToBottomButton } from "./scroll-to-bottom-button";
